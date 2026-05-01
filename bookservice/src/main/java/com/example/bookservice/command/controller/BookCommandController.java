89b04e5793ef5bd8e4707093a631dbde321da471
datnguyen122000 <datnguyen122000@gmail.com>
2026-05-01 11:01:16 +0700

    24. Áp dụng Validation Request Body cho API với Spring Boot Validation

--- bookservice/src/main/java/com/example/bookservice/command/controller/BookCommandController.java
+++ bookservice/src/main/java/com/example/bookservice/command/controller/BookCommandController.java
@@ -4,6 +4,7 @@ import com.example.bookservice.command.command.CreateBookCommand;
 import com.example.bookservice.command.command.DeleteBookCommand;
 import com.example.bookservice.command.command.UpdateBookCommand;
 import com.example.bookservice.command.model.BookRequestModel;
+import jakarta.validation.Valid;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -17,7 +18,7 @@ public class BookCommandController {
     private CommandGateway commandGateway;
 
     @PostMapping
-    public String addBook(@RequestBody BookRequestModel model){
+    public String addBook(@Valid @RequestBody BookRequestModel model){
         CreateBookCommand command = new CreateBookCommand(UUID.randomUUID().toString(), model.getName(), model.getAuthor(), true);
         return commandGateway.sendAndWait(command);
     }
--- bookservice/src/main/java/com/example/bookservice/query/model/BookResponseModel.java
+++ bookservice/src/main/java/com/example/bookservice/query/model/BookResponseModel.java
@@ -1,5 +1,7 @@
 package com.example.bookservice.query.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
