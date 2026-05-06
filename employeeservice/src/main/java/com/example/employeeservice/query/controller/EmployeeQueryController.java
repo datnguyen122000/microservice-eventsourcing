1f597676d5c1621a25b78eeceb3c33758d3b6a81
datnguyen122000 <datnguyen122000@gmail.com>
2026-05-06 22:07:24 +0700

    34. Tùy Chỉnh Và Che Dấu EndPoint với Swagger

--- employeeservice/src/main/java/com/example/employeeservice/query/controller/EmployeeQueryController.java
+++ employeeservice/src/main/java/com/example/employeeservice/query/controller/EmployeeQueryController.java
@@ -3,6 +3,9 @@ package com.example.employeeservice.query.controller;
 import com.example.employeeservice.query.model.EmployeeResponseModel;
 import com.example.employeeservice.query.queries.GetAllEmployeeQuery;
 import com.example.employeeservice.query.queries.GetDetailEmployeeQuery;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.axonframework.messaging.responsetypes.ResponseTypes;
 import org.axonframework.queryhandling.QueryGateway;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,10 +15,25 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/employees")
+@Tag(name = "Employee Query")
 public class EmployeeQueryController {
     @Autowired
     private QueryGateway queryGateway;
 
+    @Operation(
+            summary = "Get List Employee",
+            description = "Get endpoint for employee with filter",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Success"
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Unauthorized / Invalid Token"
+                    )
+            }
+    )
     @GetMapping
     public List<EmployeeResponseModel> getAllEmployee(@RequestParam(required = false, defaultValue = "false") Boolean isDisciplined) {
         GetAllEmployeeQuery query = new GetAllEmployeeQuery(isDisciplined);
