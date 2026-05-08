c9a8a08cf7a43e29b7a88624abf3354c3400fc0d
datng <datnguyen122000@gmail.com>
2026-05-08 15:32:07 +0700

    38. Cấu Hình Automation Routing Và Load Balancing

--- employeeservice/src/main/java/com/example/employeeservice/query/controller/EmployeeQueryController.java
+++ employeeservice/src/main/java/com/example/employeeservice/query/controller/EmployeeQueryController.java
@@ -6,6 +6,7 @@ import com.example.employeeservice.query.queries.GetDetailEmployeeQuery;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
 import org.axonframework.messaging.responsetypes.ResponseTypes;
 import org.axonframework.queryhandling.QueryGateway;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/employees")
 @Tag(name = "Employee Query")
+@Slf4j
 public class EmployeeQueryController {
     @Autowired
     private QueryGateway queryGateway;
@@ -36,6 +38,7 @@ public class EmployeeQueryController {
     )
     @GetMapping
     public List<EmployeeResponseModel> getAllEmployee(@RequestParam(required = false, defaultValue = "false") Boolean isDisciplined) {
+        log.info("getAllEmployee");
         GetAllEmployeeQuery query = new GetAllEmployeeQuery(isDisciplined);
         return queryGateway.query(query, ResponseTypes.multipleInstancesOf(EmployeeResponseModel.class)).join();
     }
